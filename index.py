25be4fe5619336fac50e38cafbbee6b1a4dfa308
Bruce Gordon <brucegne@gmail.com>
2019-11-29 14:34:05 -0600

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,11 @@ from flask import Flask, Response, redirect, url_for, escape, request, make_resp
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
+
+@app.route('/dailyjson', methods=['GET', 'POST'])
+def daily__json():
+    return Response("It's the daily stuff")
+
 @app.route('/<path:path>')
 def catch_all(path):
     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
