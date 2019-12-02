1576a7c0aa5d346cccda0a79387df62ac3f60eae
Bruce Gordon <brucegne@gmail.com>
2019-12-01 20:46:53 -0600

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, Response, redirect, url_for, escape, r
 from flask_redis import FlaskRedis
 
 REDIS_URL="redis://redistogo:0566827014ab8c2c76bcad1ab98239a7@angler.redistogo.com:9285/"
+# API keyE1hUaiUcOSiqc7
+# BASE ID appOEjuG867PcJetu
+from airtable import airtable
+at = airtable.Airtable('appOEjuG867PcJetu', keyE1hUaiUcOSiqc7')
+
 
 app = Flask(__name__)
 redis_client = FlaskRedis(app)
@@ -20,6 +25,12 @@ def daily__json():
         resp = make_response( render_template('index.html',**prms), 200 )
         return resp
 
+@app.route('/air')
+def air_fetch():
+    at.get('Family')
+    for r in self.at.iterate(self.Family):
+        print r
+                       
 @app.route('/<path:path>')
 def catch_all(path):
     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
