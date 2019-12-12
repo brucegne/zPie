b3568e5ddf290f0f75f65b42b5cc66b1f4036ccc
Bruce Gordon <brucegne@gmail.com>
2019-12-12 14:50:22 -0600

    Update index.py

--- index.py
+++ index.py
@@ -28,7 +28,7 @@ def add_rec():
     resp = make_response( render_template('addtest.html',**prms), 200 )
     return resp
 
-@app.route('/moderec', methods=['GET'])
+@app.route('/modrec', methods=['GET'])
 def mod_data():
     r=redis.Redis(host='angler.redistogo.com',password='0566827014ab8c2c76bcad1ab98239a7',port=9285)
     kv = request.form['created']
