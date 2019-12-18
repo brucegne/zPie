bc913f724c8730f66edddd279a582be7e7a9dd58
Bruce Gordon <brucegne@gmail.com>
2019-12-18 13:24:32 -0500

    bcr

--- index.py
+++ index.py
@@ -21,9 +21,9 @@ def ang_temp():
 def register():
     rs=redis.Redis(host='angler.redistogo.com',password='0566827014ab8c2c76bcad1ab98239a7',port=9285)
     username = b'brucegne@gmail.com'
-    password = b'Ye110wsn0w'
+    password = 'Ye110wsn0w'
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password, salt)
+    hashed = bcrypt.hashpw(password.encode(), salt)
     rs.hset('Users',username.decode(),hashed)
     return str(hashed)
 
