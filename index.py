56a132ab0b1605810016626b59d94678f678d89e
Bruce Gordon <brucegne@gmail.com>
2020-01-22 15:01:25 -0500

    form

--- index.py
+++ index.py
@@ -181,12 +181,12 @@ def mod_data():
 def add_mondo_rec():
   
     mydict = {}
-        mydict['kv'] = request.form['kv']
-        mydict['fname'] = request.form['fname']
-        mydict['lname'] = request.form['lname']
-        mydict['address'] = request.form['address']
-        mydict['city'] = request.form['city']
-        mydict['phone'] = request.form['phone']
+    mydict['kv'] = request.form['kv']
+    mydict['fname'] = request.form['fname']
+    mydict['lname'] = request.form['lname']
+    mydict['address'] = request.form['address']
+    mydict['city'] = request.form['city']
+    mydict['phone'] = request.form['phone']
     db.contacts.insert_one(mydict)
     return redirect("/", code=302)
 
